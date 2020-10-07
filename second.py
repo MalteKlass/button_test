4c35a75e05480c502374fb832deaeedfe4d22366
MalteKlass <61737087+MalteKlass@users.noreply.github.com>
2020-10-07 12:48:08 +0200

    test

--- second.py
+++ second.py
@@ -9,5 +9,9 @@ GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 while True:
     input_state = GPIO.input(18)
     if input_state == False:
-        print ("Button pressed")
-        subprocess.call(["/opt/vc/bin/tvservice", "-o"], shell=False)
+
+        subprocess.call(["/opt/vc/bin/tvservice", "-p"], shell=False)
+        subprocess.call(["sudo", "/bin/chvt", "6"], shell=False)
+        subprocess.call(["sudo", "/bin/chvt", "7"], shell=False)
+        time.sleep(1)
+
