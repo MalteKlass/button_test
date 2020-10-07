a043a6357f29fa75e651dafd26d312ab1040841c
MalteKlass <61737087+MalteKlass@users.noreply.github.com>
2020-10-07 21:08:43 +0200

    test test

--- forth.py
+++ forth.py
@@ -2,17 +2,17 @@ import subprocess
 import time
 
 a = subprocess.check_output(["/opt/vc/bin/tvservice", "-s"], shell=False)
-print ("Erster Status ist:" + a)
+print ("Erster Status ist: " + str(a))
 time.sleep(5)
 subprocess.call(["/opt/vc/bin/tvservice", "-p"], shell=False)
 subprocess.call(["sudo", "/bin/chvt", "6"], shell=False)
 subprocess.call(["sudo", "/bin/chvt", "7"], shell=False)
 b = subprocess.check_output(["/opt/vc/bin/tvservice", "-s"], shell=False)
-print ("Zweiter Status ist:" + b)
+print ("Zweiter Status ist: " + str(b))
 time.sleep(5)
 subprocess.call(["/opt/vc/bin/tvservice", "-o"], shell=False)
 c = subprocess.check_output(["/opt/vc/bin/tvservice", "-s"], shell=False)
-print ("Dritter Status ist:" + c)
+print ("Dritter Status ist: " + str(c))
 
 
 
