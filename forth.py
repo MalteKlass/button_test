0d9567ade8ecd31d169e2e59a2d89b5d4b07112a
MalteKlass <61737087+MalteKlass@users.noreply.github.com>
2020-10-07 21:04:57 +0200

    test

--- forth.py
+++ forth.py
@@ -1,5 +1,15 @@
 import subprocess
+import time
 
-a = subprocess.call(["/opt/vc/bin/tvservice", "-s"], shell=False)
+a = subprocess.check_output(["/opt/vc/bin/tvservice", "-s"], shell=False)
 print (a)
+time.sleep(5)
+subprocess.call(["/opt/vc/bin/tvservice", "-p"], shell=False)
+subprocess.call(["sudo", "/bin/chvt", "6"], shell=False)
+subprocess.call(["sudo", "/bin/chvt", "7"], shell=False)
+b = subprocess.check_output(["/opt/vc/bin/tvservice", "-s"], shell=False)
+print (b)
+
+
+
 
